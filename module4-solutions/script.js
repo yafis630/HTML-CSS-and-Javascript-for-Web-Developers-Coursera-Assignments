070bf00c097d1091f5f699e2d8909542027bf0bf
Mohd Yafis Khan <68808706+yafis630@users.noreply.github.com>
2021-05-30 23:52:27 +0530

    Update script.js

--- module4-solutions/script.js
+++ module4-solutions/script.js
@@ -10,16 +10,16 @@
 // or "j", otherwise, the program should say "Good Bye". So, the final output
 // on the console should look like this:
 /*
-Hello Yafis
-Hello Shayan
-Good Bye Jimmy
-Good Bye junaid
-Hello Pheonix
-Hello Mark
-Hello Henry
-Good Bye Jacob
-Good Bye Jhonny
-Hello Furkan
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
 WARNING!!! WARNING!!!
 The code does NOT currently work! It is YOUR job to make it work
 as described in the requirements and the steps in order to complete this
@@ -30,7 +30,7 @@ WARNING!!! WARNING!!!
 
 (function () {
 
-var names = ["Yafis", "Shayan", "Jimmy", "junaid", "Pheonix", "Mark", "Henry", "Jacob", "Jhonny", "Furkan"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
@@ -44,3 +44,4 @@ for (var i = 0; i < names.length; i++) {
 }
 
 })();
+
